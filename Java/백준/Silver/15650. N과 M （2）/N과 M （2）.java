312d907dd5b6e6666b60a7c511022f654685bd4e
hungryTiger-roar <gksals520@gmail.com>
2025-08-28 17:23:49 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 15900 KB -BaekjoonHub

--- Java/백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ Java/백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,49 +1,55 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static int[] num;
-	static int[] result;
-	
-	public static void comb(int depth, int numIdx)
-	{
-		if(depth == M)
-		{
-			for(int i=0; i<result.length; i++) {
-				System.out.print(result[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		// 1부터 N까지!!
-		for(int i=numIdx; i<num.length; i++)
-		{
-			result[depth] = num[i];
-			// i+1로 중복 방지!!!!!!!!!!!!!!
-			comb(depth+1, i+1);
-		}
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String[] input = sc.nextLine().split(" ");
-		N = Integer.parseInt(input[0]);
-		M = Integer.parseInt(input[1]);
-		
-		// 숫자 배열 채우기 0~N
-		num = new int[N+1];
-		for(int i=0; i<num.length; i++)
-		{
-			num[i] = i;
-		}
-		// 결과 배열 만들기
-		result = new int[M];
-		
-		comb(0,1);
-	}
+
+    static int[] perm;
+    static boolean[]visited;
+    static int N;
+    static int M;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        perm = new int[M];
+        visited = new boolean[N+1];
+
+        combination(0, 1);
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void combination(int depth, int start) throws IOException {
+
+        if (depth == M) {
+
+            for (int i = 0; i < M; i++) {
+                bw.write(perm[i] + " ");
+            }
+
+            bw.newLine();
+
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+
+            perm[depth] = i;
+            
+            combination(depth + 1, i + 1); 
+        }
+    }
+
 }
