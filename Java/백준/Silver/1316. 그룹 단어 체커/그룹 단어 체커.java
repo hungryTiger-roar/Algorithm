bc2a5bf684243ecd2f31668d9573a2ec927239d3
hungryTiger-roar <gksals520@gmail.com>
2025-10-25 22:28:36 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- Java/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ Java/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -20,34 +22,20 @@ public class Main {
 			char[] input = br.readLine().toCharArray();
 			// 입력된 단어에서 연속되는 문자는 빼고 리스트에 넣어주기
 			ArrayList<Character> al = new ArrayList<>();
+			// 중복된 단어 모두 빼기
+			Set<Character> set = new HashSet<>();
 			al.add(input[0]);
 			for(int j=1; j<input.length; j++)
 			{
-				if(input[j-1] != input[j])
-				{
-					al.add(input[j]);
-				}
+				if(input[j-1] != input[j]) al.add(input[j]); // 리스트는 연속되는 경우가 아니면 넣어주기
 			}
+			// 셋은 그냥 추가해주면 중복된 값은 알아서 빼줌
+			for(int j=0; j<input.length; j++) set.add(input[j]); 
 			
-			// a~z => 97~122 총 26개
-			// 압축된 al이 알파벳 별로 몇개 있는지 카운트하기
-			int[] cnt = new int[26];
-			for(int j=0; j<al.size(); j++)
+			if(set.size() == al.size()) // 다른 경우는 그룹단어가 아님
 			{
-				cnt[((int)al.get(j)) - 97]++; // a는 0번 인덱스에, z는 25번 인덱스에 +1 해주기
+				result++;
 			}
-			
-            // 그룹단어 여부
-			boolean isGroupWord = true;
-			for(int j=0; j<cnt.length; j++)
-			{
-				if(cnt[j] > 1) // 그룹단어가 아닌경우
-				{
-					isGroupWord = false;
-					break;
-				}
-			}
-			if(isGroupWord) result++; // 그룹단어이면 +1 해주기
 		}
 		sb.append(result);
 		bw.write(sb.toString());
