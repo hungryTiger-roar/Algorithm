e1402dbcdc67bcc11ebc4277bf0642958f3ca366
hungryTiger-roar <gksals520@gmail.com>
2025-10-27 17:34:35 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 176 ms, Memory: 17928 KB -BaekjoonHub

---
+++ Java/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		
+		for(int i=-999; i<=999; i++)
+		{
+			for(int j=-999; j<=999; j++)
+			{
+				if(a*i + b*j == c && d*i + e*j == f) sb.append(i).append(" ").append(j);
+			}
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
