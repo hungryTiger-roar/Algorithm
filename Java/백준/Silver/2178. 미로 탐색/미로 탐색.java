bd8a74ae0b5897dfaa93f4fbdc9f93aa2a0e3917
hungryTiger-roar <gksals520@gmail.com>
2025-11-17 23:39:48 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14664 KB -BaekjoonHub

--- Java/백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ Java/백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -11,16 +11,16 @@ public class Main {
 	static int N, M;
 	static int[][] map;
 	static boolean[][] visited;
-	static int[] dx = {-1, 1, 0, 0};
+	static int[] dx = {-1, 1, 0, 0}; // 상하좌우
 	static int[] dy = {0, 0, -1, 1};
 	
 	static void bfs()
 	{
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {0,0}); // 시작점 넣기
-		visited[0][0] = true;
+		Queue<int[]> q = new LinkedList<>(); // 큐생성
+		q.offer(new int[]{0,0}); // 시작점 큐에 넣기
+		visited[0][0] = true; // 시작점 방문처리
 		
-		while(!q.isEmpty())
+		while(!q.isEmpty()) // 큐가 비어있게 될 때까지 반복
 		{
 			int[] cur = q.poll();
 			
@@ -28,18 +28,19 @@ public class Main {
 			{
 				int nx = cur[0] + dx[i];
 				int ny = cur[1] + dy[i];
-				if(nx>=0 && nx<N && ny>=0 && ny<M
-						&& !visited[nx][ny] && map[nx][ny]==1)
+				
+				if(nx>=0 && nx<N && ny>=0 && ny<M // 맵의 범위 안벗어나기
+						&& map[nx][ny] == 1 && !visited[nx][ny]) // 길이고, 방문하지 않은 곳
 				{
+					map[nx][ny] = map[cur[0]][cur[1]] + 1; // 옮기기 전 자리에서 +1 해주기
 					q.offer(new int[] {nx, ny});
-					map[nx][ny] = map[cur[0]][cur[1]]+1;
 					visited[nx][ny] = true;
 				}
 			}
 		}
 	}
 	
-	public static void main(String[] args) throws IOException  {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
@@ -47,18 +48,22 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N][M];
+		
+		// 초기화
 		map = new int[N][M];
+		visited = new boolean[N][M];
+		
 		for(int i=0; i<N; i++)
 		{
-			char[] input = br.readLine().toCharArray();
+			String input = br.readLine();
 			for(int j=0; j<M; j++)
 			{
-				map[i][j] = input[j]-'0';
+				map[i][j] = input.charAt(j)-'0';
 			}
 		}
 		
 		bfs();
+		
 		sb.append(map[N-1][M-1]);
 		bw.write(sb.toString());
 		bw.flush();
